7b28e155c42707c3fe92d065d99d9e433ce4bca2
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2025-02-19 23:12:17 +0330

    init 2

--- main.lua
+++ main.lua
@@ -1,7 +1,8 @@
 -- name: Mario In The Multiverse
 -- description: this is, elon musk
 -- incompatible: romhack
-
+gLevelValues.entryLevel = LEVEL_BOB
+gLevelValues.disableActs = true
 smlua_audio_utils_replace_sequence(0x25, 42, 80, "25_custom_peanut_plains")
 
 function geo_update_mverse_pipe()
