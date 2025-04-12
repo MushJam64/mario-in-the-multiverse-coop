3b7096ab542ff51f2c4fe1589447c0416cb32b20
MaiskX3 <maiskxdxd@gmail.com>
2025-04-12 10:48:39 -0400

    m64s

--- main.lua
+++ main.lua
@@ -5,8 +5,8 @@
 gLevelValues.entryLevel = LEVEL_CASTLE --LEVEL CASTLE and bob if no stars
 gLevelValues.disableActs = true
 
-smlua_audio_utils_replace_sequence(0x25, 19, 80, "25_custom_peanut_plains") --OG BANK EXTENDED (42)
-smlua_audio_utils_replace_sequence(SEQ_CUSTOM_KIRBY_BOSS, 19, 80, "26_custom_kirby_boss") --OG BANK EXTENDED (42)
+smlua_audio_utils_replace_sequence(0x25, 42, 80, "25_custom_peanut_plains") --OG BANK EXTENDED (42)
+smlua_audio_utils_replace_sequence(SEQ_CUSTOM_KIRBY_BOSS, 42, 80, "26_custom_kirby_boss") --OG BANK EXTENDED (42)
 smlua_audio_utils_replace_sequence(0x4e, 42, 80, "4E_mitm_hub")
 smlua_audio_utils_replace_sequence(0x27, 42, 80, "27_custom_save_hut")
 
