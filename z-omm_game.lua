d511d4b70ba393aa094ca2dfdf2cc813f9c4dfd8
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2025-06-08 23:42:10 +0330

    f

--- z-omm_game.lua
+++ z-omm_game.lua
@@ -1,38 +1,50 @@
 ---@diagnostic disable: undefined-field, undefined-global
-if _G.OmmEnabled then
-    _G.OmmApi.omm_register_game("Mario in The Multiverse ", function() return true end, function()
-        ---------------
-        -- Game data --
-        ---------------
 
-        _G.OmmApi.omm_register_game_data(-1, 0, LEVEL_NONE, true, true, 0xFFFF00, 500)
+local truth_and_real = false
 
-        -----------------
-        -- Level stars --
-        -----------------
-        
-        --------------------
-        -- Star behaviors --
-        --------------------
-        
-        _G.OmmApi.omm_register_star_behavior(bhvStarPieceSwitch, "Star Piece", "STAR PIECE", function(bhvParams) return true end)
-        _G.OmmApi.omm_register_star_behavior(bhvFightWavesManager, "Fighting Waves", "FIGHTING WAVES", function(bhvParams) return true end)
-        _G.OmmApi.omm_register_star_behavior(bhvCrane, "Crane", "CRANE", function(bhvParams) return true end)
+local function update_omm()
+    if _G.OmmEnabled and not truth_and_real then
+        --djui_popup_create("s", 1)
+        _G.OmmApi.omm_register_game("Mario in The Multiverse ", function() return true end, function()
+            ---------------
+            -- Game data --
+            ---------------
 
-        --------------------
-        -- Camera presets --
-        --------------------
+            _G.OmmApi.omm_register_game_data(-1, 0, LEVEL_NONE, true, true, 0xFFFF00, 500)
+            _G.OmmApi.omm_force_setting("hud", 3)
 
-        -------------------------
-        -- Camera no-col boxes --
-        -------------------------
+            -----------------
+            -- Level stars --
+            -----------------
 
-        ----------------
-        -- Warp pipes --
-        ----------------
+            --------------------
+            -- Star behaviors --
+            --------------------
 
-        -------------------
-        -- Non-Stop mode --
-        -------------------
-    end)
+            _G.OmmApi.omm_register_star_behavior(bhvStarPieceSwitch, "Star Piece", "STAR PIECE",
+                function(bhvParams) return true end)
+            _G.OmmApi.omm_register_star_behavior(bhvFightWavesManager, "Fighting Waves", "FIGHTING WAVES",
+                function(bhvParams) return true end)
+            _G.OmmApi.omm_register_star_behavior(bhvCrane, "Crane", "CRANE", function(bhvParams) return true end)
+
+            --------------------
+            -- Camera presets --
+            --------------------
+
+            -------------------------
+            -- Camera no-col boxes --
+            -------------------------
+
+            ----------------
+            -- Warp pipes --
+            ----------------
+
+            -------------------
+            -- Non-Stop mode --
+            -------------------
+        end)
+        truth_and_real = true
+    end
 end
+
+hook_event(HOOK_UPDATE, update_omm)
